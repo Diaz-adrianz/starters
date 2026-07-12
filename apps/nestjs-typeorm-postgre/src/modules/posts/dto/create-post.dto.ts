004615c3253e6f9b8dc48cb4d266f35fa2aa256b
Diaz-Adrianz <diazz.developer@gmail.com>
2026-07-12 21:02:05 +0700

    feat: basic crud

--- apps/nestjs-typeorm-postgre/src/modules/posts/dto/create-post.dto.ts
+++ apps/nestjs-typeorm-postgre/src/modules/posts/dto/create-post.dto.ts
@@ -1,1 +1,9 @@
-export class CreatePostDto {}
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreatePostDto {
+  @MinLength(3)
+  @MaxLength(500)
+  @IsString()
+  @IsNotEmpty()
+  content: string;
+}
--- apps/nestjs-typeorm-postgre/src/modules/posts/posts.service.ts
+++ apps/nestjs-typeorm-postgre/src/modules/posts/posts.service.ts
@@ -1,26 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Post } from './entities/post.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class PostsService {
+  constructor(@InjectRepository(Post) private postRepo: Repository<Post>) {}
+
   create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+    return this.postRepo.insert(createPostDto);
   }
 
   findAll() {
-    return `This action returns all posts`;
+    return this.postRepo.findAndCount();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} post`;
+  findOne(id: string) {
+    return this.postRepo.findOneOrFail({ where: { id } });
   }
 
-  update(id: number, updatePostDto: UpdatePostDto) {
-    return `This action updates a #${id} post`;
+  update(id: string, updatePostDto: UpdatePostDto) {
+    return this.postRepo.update({ id }, updatePostDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} post`;
+  remove(id: string) {
+    return this.postRepo.delete({ id });
   }
 }
