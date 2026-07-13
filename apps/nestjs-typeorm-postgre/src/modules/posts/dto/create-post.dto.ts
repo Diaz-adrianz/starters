1b255470c135fc00758cbfad5236c4397f2877fd
Diaz-Adrianz <diazz.developer@gmail.com>
2026-07-13 20:35:59 +0700

    feat: remove post resouce

--- apps/nestjs-typeorm-postgre/src/modules/posts/dto/create-post.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
-
-export class CreatePostDto {
-  @MinLength(3)
-  @MaxLength(500)
-  @IsString()
-  @IsNotEmpty()
-  content: string;
-}
