0e1392e935f02c0681b589dd9fcb09806a965459
Diaz-Adrianz <diazz.developer@gmail.com>
2026-08-21 19:43:37 +0700

    feat(auth): adjust user cache hit

--- apps/nestjs-typeorm-postgre/src/modules/auth/strategies/jwt.strategy.ts
+++ apps/nestjs-typeorm-postgre/src/modules/auth/strategies/jwt.strategy.ts
@@ -1,26 +1,27 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { Inject, Injectable } from '@nestjs/common';
+import { ForbiddenException, Inject, Injectable } from '@nestjs/common';
 import { JwtTokenPayload } from '../interfaces/jwt-payload.interface';
 import { Principal } from '../../../shared/classes/principal.class';
-import { AuthService } from '../auth.service';
 import { AUTH_CONFIG_KEY, type AuthConfig } from '../../../config/auth.config';
 import { CacheService } from '../../../infra/cache/cache.service';
 import { LoggerService } from '../../../infra/logger/logger.service';
-import { UserService } from '../../identity/resources/user/user.service';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { AppDataSource } from '../../../database/typeorm/app-data-source';
+import { DatabaseKeys } from '../../../database/database-keys.contant';
+import { User } from '../../identity/entities/user.entity';
 
 type UserCache = {
   id: string;
   username: string;
-  roles: string[];
+  roles: [string, string][];
 };
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(
     @Inject(AUTH_CONFIG_KEY) private authConfig: AuthConfig,
-    private userService: UserService,
-    private authService: AuthService,
+    @InjectDataSource(DatabaseKeys.DEFAULT) private dataSource: AppDataSource,
     private cache: CacheService,
     private logger: LoggerService,
   ) {
@@ -36,14 +37,23 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
 
     if (!userCache) {
       this.logger.log('User cache missed', this.constructor.name);
+      const userRepo = this.dataSource.getRepository(User);
 
-      const user = await this.userService.findById(payload.sub);
-      this.authService.checkUserActive(user);
+      const user = await userRepo.findOneOrFail({
+        where: { id: payload.sub },
+        relations: { roles: { role: true } },
+        select: {
+          ...userRepo.select('*'),
+          roles: { id: true, role: { id: true, name: true } },
+        },
+      });
+      if (!user.isActive())
+        throw new ForbiddenException('Account suspended or not verified yet');
 
       userCache = {
         id: user.id,
         username: user.username,
-        roles: user.roles.map((r) => r.role.id) ?? [],
+        roles: user.roles.map((r) => [r.role.id, r.role.name]) ?? [],
       };
 
       await this.cache.set((k) => k.user(payload.sub), userCache, 0);
@@ -52,7 +62,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     const principal = new Principal(
       { id: userCache.id, username: userCache.username },
       { id: payload.sid },
-      userCache.roles.map((rId) => ({ id: rId })),
+      userCache.roles.map((role) => ({ id: role[0], name: role[1] })),
     );
 
     return principal;
--- apps/nestjs-typeorm-postgre/src/shared/classes/principal.class.ts
+++ apps/nestjs-typeorm-postgre/src/shared/classes/principal.class.ts
@@ -6,7 +6,7 @@ export class Principal {
   constructor(
     public readonly user: { id: string; username: string },
     public readonly session: { id: string },
-    public readonly roles: { id: string }[],
+    public readonly roles: { id: string; name: string }[],
   ) {}
 
   toSubject() {
